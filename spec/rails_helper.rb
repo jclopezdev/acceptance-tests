a205d358dc2b2087f3e9b13c367a7c647021dc1f
Juan Carlos López <jclopezdev@gmail.com>
2019-01-15 13:53:06 -0600

    Testing emails and config letter_opener gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.before { ActionMailer::Base.deliveries.clear }
 end
